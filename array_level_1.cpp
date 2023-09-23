12fbc8ade1f9806d05a06192c174c03cdc185198
Pratik_Dhadave <pratik007ddd@gmail.com>
2023-09-23 22:36:05 +0530

    count 0s & 1s in array, find min & max no in array, reversed the given array

--- array_level_1.cpp
+++ array_level_1.cpp
@@ -1,6 +1,58 @@
 #include <iostream>
+#include<limits.h>
 using namespace std;
 
+bool linerSearch(int arr[], int size, int target){
+    for(int i=0; i<size; i++){
+        if(arr[i]==target)
+        return true;
+    }
+    return false;
+}
+
+void countZeroOne(int arr[] ,int size){
+    int zeroCount =0;
+    int oneCount =0;
+    for(int i=0; i<size; i++){
+        if(arr[i] == 0){
+            zeroCount++;
+        }else if(arr[i] ==1){
+            oneCount++;
+        }
+    }
+
+    cout<<endl<<"zero count is:"<<zeroCount;
+    cout<<endl<<"one count is:"<<oneCount;
+}
+
+int findMinimum(int arr[], int size){
+    int minAns = INT_MAX;
+    for(int i=0; i<size; i++){
+        if(arr[i]<minAns){
+            minAns=arr[i];
+        }
+    }
+    return minAns;
+}
+
+void reversedArray(int arr[], int size){
+    int left = 0; 
+    int right= size-1;
+    // using for loop
+    // for(int left=0, right=size-1; left<=right; left++,right--){
+    //     swap(arr[left],arr[right]);
+    // }
+    while(left<=right){
+        swap(arr[left] , arr[right]);
+        left++;
+        right--;
+
+    }
+
+    for(int i=0; i<size; i++){
+        cout<<arr[i]<<"  ";
+    }
+}
 int main(){
     // int a=5;
     // cout<<&a<<endl<<sizeof(a);
@@ -56,27 +108,57 @@ int main(){
     // cout<<totalSum;
 
     //problem statement: Linear Search
-    int arr[5];
-    bool flag=0;
-    cout<<"Enter value:"<<endl;
-    for(int i=0; i<5; i++){
-        cin>>arr[i];
-    }
+    // int arr[5];
+    // bool flag=0;
+    // cout<<"Enter value:"<<endl;
+    // for(int i=0; i<5; i++){
+    //     cin>>arr[i];
+    // }
 
-    int target;
-    cout<<endl<<"Enter target value:";
-    cin>>target;
+    // int target;
+    // cout<<endl<<"Enter target value:";
+    // cin>>target;
 
-    for(int i=0; i<5; i++){
-        if(arr[i]==target){
-            flag =1;
-            break;
-        }
-    }
-    if(flag == 1){
-        cout<<endl<<"target found";
-    }else{
-        cout<<"target not found";
-    }
+    // for(int i=0; i<5; i++){
+    //     if(arr[i]==target){
+    //         flag =1;
+    //         break;
+    //     }
+    // }
+    // if(flag == 1){
+    //     cout<<endl<<"target found";
+    // }else{
+    //     cout<<"target not found";
+    // }
+
+    // 2nd solution without using flag
+
+    // int arr[5]= {2,3,44,65,1};
+    // int size =5;
+    // int target = 443;
+    // bool ans = linerSearch(arr,size,target);
+    // if(ans == 1)
+    // cout<<"target found";
+    // else
+    // cout<<"target not found";
+
+    //problem statement : count 0`s & 1`s in an array
+    // int arr[] = {1,0,0,1,0,0,1,1,1,0,0,0};
+    // int size=12;
+    // countZeroOne(arr,size);
+    
+    //problem statement: find minimum no in array
+    // int arr[]={24,3,23,-4,5,6};
+    // int size = 6;
+    // int minimum = findMinimum(arr,size);
+    // cout<<endl<<"minimum no is:"<<minimum;
+
+    //problem statement:reversed array
+
+    int arr[] = {10,20,30,40,50,60,70};
+    int size = 7;
+    reversedArray(arr,size);
+
+
     return 0;
 }
