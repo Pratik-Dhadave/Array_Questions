fa10012582f453fee2c65b370106c75f2c7612c3
Pratik_Dhadave <pratik007ddd@gmail.com>
2023-10-23 17:26:40 +0530

    different vector operations, 2D vecto & jagged vector

--- vector_level_1.cpp
+++ vector_level_1.cpp
@@ -60,6 +60,14 @@ void printVector(vector<int>v){
         cout<<v.at(i)<<"   ";
     }
 }
+
+void printVector2(vector<char>ch){
+    cout<<endl<<"2nd Method  :";
+    // 2nd method of printing vector
+    for(auto it:ch){
+        cout<<it<<"      ";
+    }
+}
 int main(){
 
     // vector<int>v;
@@ -95,11 +103,13 @@ int main(){
     vector<int>arr(5,-2);
     arr.push_back(60);
     printVector(arr);
+    cout<<endl<<"size"<<arr.size()<<"capacity"<<arr.capacity();
 
     // vector<int>arr1 = {6,7,8,90,45};
     vector<int>arr1{6,7,8,90,45};
     arr1.pop_back();
     printVector(arr1);
+    cout<<endl<<"size"<<arr1.size()<<"capacity"<<arr1.capacity();
 
 
     //how to copy vector 
@@ -111,5 +121,63 @@ int main(){
     brr1.push_back(321);
     brr1.push_back(6677);
     printVector(brr1);
+
+    cout<<endl;
+    vector<char>ch;
+    ch.push_back('A');
+    ch.push_back('B');
+    ch.push_back('C');
+    ch.push_back('D');
+    ch.push_back('E');
+
+    cout<<"Front Elelment:"<<ch[0]<<endl;
+    cout<<"Front Elelment:"<<ch.front()<<endl;
+    cout<<"Last Elelment:"<<ch.back()<<endl;
+    cout<<"Last Elelment:"<<ch[ch.size()-1]<<endl;
+
+    printVector2(ch);
+
+
+
+    // 2D Vector
+
+    vector< vector<int> >v1(5,vector<int>(3,100));
+    for(int i=0; i<v1.size(); i++){
+        for(int j=0; j<v1[i].size(); j++){
+            cout<<v1[i][j]<<"  ";
+        }
+        cout<<endl;
+    }
+    // vector<char>v(10, 'A');
+    cout<<endl<<"Using forEach method:"<<endl;
+    for(auto row:v1){
+        for(auto col:row){
+            cout<<col<<" ";
+        }
+        cout<<endl;
+    }
+
+    //jagged array
+
+    vector< vector<int> >jagg;
+
+    vector<int>j1(10,2);
+    vector<int>j2(8,4);
+    vector<int>j3(6,6);
+    vector<int>j4(4,8);
+    vector<int>j5(2,10);
+
+    jagg.push_back(j1);
+    jagg.push_back(j2);
+    jagg.push_back(j3);
+    jagg.push_back(j4);
+    jagg.push_back(j5);
+
+    for(int i=0; i<jagg.size(); i++){
+        for(int j=0; j<jagg[i].size(); j++){
+            cout<<jagg[i][j]<<"   ";
+        }
+        cout<<endl;
+    }
     return 0;
 }
